fd47ed6cece39113387cc46d08abac774f50e334
jix <josagoro2000@gmail.com>
2024-08-30 00:11:42 +0200

    add a clean way to get customer api url

--- services/authentication.py
+++ services/authentication.py
@@ -1,14 +1,16 @@
+from decouple import config
 import requests
 from ninja.security import APIKeyHeader
 from schemas.types import AuthResponse
 
+API_BASE_URL = config('CUSTOMER_API_URL')
 
 class ApiKey(APIKeyHeader):
     param_name = "x-access-token"
 
     def verify_token(self, auth_token: str):
         result = requests.get(
-            "http://localhost:3000/api/auth/verify-token",
+            f"{API_BASE_URL}/api/auth/verify-token",
             headers={
                 "x-access-token": auth_token
             }
