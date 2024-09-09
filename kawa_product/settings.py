6b91a634453b101bb1f3f14ccad547243ac5624e
jix <josagoro2000@gmail.com>
2024-09-09 21:42:56 +0200

    Allowed host issues + move from decouple to os.getenv + acl list

--- kawa_product/settings.py
+++ kawa_product/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ayv*rgaj7!w(w%nh$0!4r@yzr+twkq!t2qb7^@tng5jkh05#da
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = getenv('ALLOWED_HOSTS').split(",")
+ALLOWED_HOSTS = getenv('ACL_LIST').split(",")
 
 DJANGO_SETTINGS_MODULE = "kawa_product.settings"
 
